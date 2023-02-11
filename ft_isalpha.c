a7c06b343b17254dfaea90d34e9f6c619ada98a9
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-11 14:24:26 -0300

    add função ft_isalpha.c

---
+++ ft_isalpha.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                            */
+/*   ft_isalpha.c                                                             */
+/*                                                                            */
+/* Recebe um único argumento (caractere) que será convertido para inteiro.    */
+/* Mesmo que isalpha() receba um inteiro como argumento, o caractere é        */
+/*passado para a função. Internamente, o caractere é convertido em seu valor  */
+/*ASCII para a verificação. Retorna: 1 caso seja uma letra do alfabeto        */
+/* maiúscula e 0 caso não esteja no alfabeto.                                 */                                                                      
+/*                                                                            */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) ||
+		(c >= 97 && c <= 122))
+		return (1);
+	return (0);
+}
