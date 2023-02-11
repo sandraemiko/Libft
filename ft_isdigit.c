928c600121037e421fe8e956ae779aba60e2e188
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-11 14:43:49 -0300

    add função ft_isdigit.c

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,1 +1,22 @@
-dd
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                            */
+/*   ft_isdigit.c                                                             */
+/*                                                                            */
+/* Recebe: um único argumento (caractere) que será convertido para inteiro.   */
+/* Mesmo que isdigit() receba um inteiro como argumento, o caractere é passado*/
+/* para a função. Internamente, o caractere é convertido em seu valor ASCII   */
+/* para a verificação. Retorna o valor 1 quando o caractere recebido é um     */
+/* dígito de 0 a 9 ou 0 caso contrário.                                       */                                                                      
+/*                                                                            */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
