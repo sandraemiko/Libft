6f15de95a9d8bbd58214972a698d64049be229b9
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-11 18:09:00 -0300

    add a função ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -13,9 +13,9 @@
 int ft_strncmp (const char* s1, const char* s2, size_t n)
 {
 	size_t	i;
+
 	i = 0;
-    
-	while ((*(s1 + i) == *(s2 + i)) && i < n)
+    	while ((*(s1 + i) == *(s2 + i)) && i < n)
 	{
 		if (*(s1 + i) == '\0')
         {
