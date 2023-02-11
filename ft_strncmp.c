989769e9aeef69c856c90c804d27d269c510aa31
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-11 14:04:49 -0300

    add função ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -13,7 +13,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int strncmp_novo(const char* s1, const char* s2, size_t n)
+int ft_strncmp (const char* s1, const char* s2, size_t n)
 {
 
 	size_t	i;
