b2fd6dbe4b58e2cb7af7380ad4913550238ba292
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-12 10:20:58 -0300

    add a função ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,20 @@
+/* ******************************************************************************* */
+/*                                                                                 */
+/*                                                             :::      ::::::::   */
+/*   ft_memcpy.c                                             :+:      :+:    :+:   */
+/*                                                         +:+ +:+         +:+     */
+/*   By: sandraemiko<sandraemiko@prof.educacao.sp.gov.br> +#+  +:+     +#+         */
+/*                                                     +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/12 10:19:04 by sandraemiko            #+#    #+#             */
+/*                                                         ###   ########.fr       */
+/*                                                                                 */
+/* ******************************************************************************* */
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	while (n --)
+	{
+		*(unsigned char*)(dest + n) = *(unsigned char*)(src + n);
+	}
+	return (dest);
+}
