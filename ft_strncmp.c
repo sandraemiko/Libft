4e53c7def1a89192e0d2dd71c3c97f9574675c99
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-12 19:32:23 -0300

    add a função ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -5,27 +5,21 @@
 /*                                                         +:+ +:+         +:+     */
 /*   By:sandraemiko<sandraemiko@prof.educacao.sp.gov.br>  +#+  +:+     +#+         */
 /*                                                     +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/11 17:31:03 by sandraemiko            #+#    #+#             */
+/*   Created: 2023/02/12 19:31:03 by sandraemiko            #+#    #+#             */
 /*                                                         ###   ########.fr       */
 /*                                                                                 */
 /* ******************************************************************************* */
 
-int ft_strncmp (const char* s1, const char* s2, size_t n)
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t	index;
 
-	i = 0;
-    	while ((*(s1 + i) == *(s2 + i)) && i < n)
+	index = 0;
+	while ((*(s1 + index) != '\0' || *(s2 + index) != '\0') && index < n)
 	{
-		if (*(s1 + i) == '\0')
-        {
-            return (0);
-        }
-        i++;
+		if (*(s1 + index) != *(s2 + index))
+			return (*(unsigned char*)(s1 + index) - *(unsigned char*)(s2 + index));
+		index++;
 	}
-    if (n == i)
-    {
-        return (0);
-    }
-	return (*(unsigned char*)(s1 + i) - *(unsigned char*)(s2 + i));
+	return (0);
 }
