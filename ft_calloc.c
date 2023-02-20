d926cf0bc8f78c96a16a13e0a1d5a0fd19cb3172
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-20 11:59:53 -0300

    add a função ft_callov.c

---
+++ ft_calloc.c
@@ -0,0 +1,22 @@
+/* ******************************************************************************* */
+/*                                                                                 */
+/*                                                             :::      ::::::::   */
+/*   ft_calloc.c                                             :+:      :+:    :+:   */
+/*                                                         +:+ +:+         +:+     */
+/*   By: sandraemiko<sandraemiko@prof.educacao.sp.gov.br> +#+  +:+     +#+         */
+/*                                                     +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 11:19:04 by sandraemiko            #+#    #+#             */
+/*                                                         ###   ########.fr       */
+/*                                                                                 */
+/* ******************************************************************************* */
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char *pointer;
+
+	pointer = malloc (nmemb * size);
+	if (!pointer)
+		return NULL;
+	ft_bzero (pointer, (nmemb * size));
+	return (pointer);
+}
