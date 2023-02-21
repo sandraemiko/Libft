945d83c74333df5e9da4e5c659ce5d28043ee0f8
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-21 13:35:30 -0300

    add a função ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,24 @@
+/* ******************************************************************************* */
+/*                                                                                 */
+/*                                                             :::      ::::::::   */
+/*   ft_strjoin.c                                            :+:      :+:    :+:   */
+/*                                                         +:+ +:+         +:+     */
+/*   By: sandraemiko<sandraemiko@prof.educacao.sp.gov.br> +#+  +:+     +#+         */
+/*                                                     +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 20:09:04 by sandraemiko            #+#    #+#             */
+/*                                                         ###   ########.fr       */
+/*                                                                                 */
+/* ******************************************************************************* */
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (str != NULL)
+	{
+		ft_strlcpy(str, s1, (ft_strlen(s1) + 1));
+		ft_strlcat(str, s2, (ft_strlen(s1) + ft_strlen(s2) + 1));
+	}
+	return (str);
+}
