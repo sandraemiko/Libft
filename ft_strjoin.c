f72d3eab18121519351052356f8035635293d0ee
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-21 18:45:45 -0300

    add a função ft_srtjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -16,7 +16,7 @@ char *ft_strjoin(char const *s1, char const *s2)
 	str = (char*)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (str != NULL)
 	{
-		ft_strlcpy(str, s1, ft_strlen(s1)0 + 1);
+		ft_strlcpy(str, s1, ft_strlen(s1) + 1);
 		ft_strlcat(str, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
 	}
 	return (str);
