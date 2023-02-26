3250e8a6f37656ee1a2eafc8fcc1c28f0f157f5f
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-26 05:02:03 -0300

    add a função ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ******************************************************************************* */
+/*                                                                                 */
+/*                                                             :::      ::::::::   */
+/*   ft_strmapi.c                                            :+:      :+:    :+:   */
+/*                                                         +:+ +:+         +:+     */
+/*   By:sandraemiko<sandraemiko@prof.educacao.sp.gov.br>  +#+  +:+     +#+         */
+/*                                                     +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/26 16:07:03 by sandraemiko            #+#    #+#             */
+/*                                                         ###   ########.fr       */
+/*                                                                                 */
+/* ******************************************************************************* */
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	if (s == NULL || f == NULL)
+		return (NULL);
+	str = (char*)malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	while (*(char*)(s + i) != '\0')
+	{
+		*(str + i) = f(i, *(char*)(s + i));
+		i++;
+	}
+	*(str + i) = '\0';
+	return (str);
+}
