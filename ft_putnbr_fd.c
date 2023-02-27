7218dc74ca42dcc30982a23aaa3bcefdf6891ea6
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-27 18:48:45 -0300

    add libft.h

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,9 +6,11 @@
 /*   By:sandraemiko<sandraemiko@prof.educacao.sp.gov.br>  +#+  +:+     +#+         */
 /*                                                     +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 19:07:03 by sandraemiko            #+#    #+#             */
-/*                                                         ###   ########.fr       */
+/*   updated: 2023/02/27 21:22:10 by sandraemiko           ###   ########.fr       */
 /*                                                                                 */
 /* ******************************************************************************* */
+
+#include "libft.h"
 
 void ft_putnbr_fd(int n, int fd)
 {
