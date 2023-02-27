cda33f2b781e4b80ad1741bce8273ea904837d41
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-27 19:35:40 -0300

    add libft.h

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,13 +6,13 @@
 /*   By: sandraemiko<sandraemiko@prof.educacao.sp.gov.br> +#+  +:+     +#+         */
 /*                                                     +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 17:31:03 by sandraemiko            #+#    #+#             */
-/*                                                         ###   ########.fr       */
+/*   Updated: 2023/02/27 21:22:10 by sandraemiko           ###   ########.fr       */
 /*                                                                                 */
 /* ******************************************************************************* */
 
 #include "libft.h"
 
-int		ft_isalnum(int c)
+int   ft_isalnum(int c)
 {
    if (ft_isalpha(c) || ft_isdigit(c))
         return (1);
