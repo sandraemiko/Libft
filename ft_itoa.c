ad98c9f3940dc541dd09979170d034812bbc6b87
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-27 19:41:02 -0300

    add libft.h

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,13 +6,13 @@
 /*   By:sandraemiko<sandraemiko@prof.educacao.sp.gov.br>  +#+  +:+     +#+         */
 /*                                                     +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 21:13:03 by sandraemiko            #+#    #+#             */
-/*                                                         ###   ########.fr       */
+/*   Updated: 2023/02/27 21:22:10 by sandraemiko           ###   ########.fr       */
 /*                                                                                 */
 /* ******************************************************************************* */
 
 #include "libft.h"
 
-static int ft_contdig(int n)
+static int	ft_contdig(int n)
 {
 	int cont;
 
