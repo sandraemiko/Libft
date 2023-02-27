fadb3e9391b7b9815c657a076d80fff08b8e454b
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-27 19:14:27 -0300

    add libft.h

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,9 +6,11 @@
 /*   By: sandraemiko<sandraemiko@prof.educacao.sp.gov.br> +#+  +:+     +#+         */
 /*                                                     +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 17:31:03 by sandraemiko            #+#    #+#             */
-/*                                                         ###   ########.fr       */
+/*   Updated: 2023/02/27 21:22:10 by sandraemiko           ###   ########.fr       */
 /*                                                                                 */
 /* ******************************************************************************* */
+
+#include "libft.h"
 
 size_t		ft_strlen(const char *s)
 {
