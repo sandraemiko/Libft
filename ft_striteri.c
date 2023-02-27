7e78a579f2eb3cf7a7457a6c1ca5df32469b1d4a
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-27 19:01:53 -0300

    add libft.h

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,9 +6,11 @@
 /*   By:sandraemiko<sandraemiko@prof.educacao.sp.gov.br>  +#+  +:+     +#+         */
 /*                                                     +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 19:07:03 by sandraemiko            #+#    #+#             */
-/*                                                         ###   ########.fr       */
+/*   Updated: 2023/02/27 21:22:10 by sandraemiko           ###   ########.fr       */
 /*                                                                                 */
 /* ******************************************************************************* */
+
+#include "libft.h" 
 
 void ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
