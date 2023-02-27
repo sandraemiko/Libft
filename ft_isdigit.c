937c75a4f85fbfc5ff1672597adf4047276e4243
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-27 19:39:25 -0300

    add libft.h

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,13 +6,13 @@
 /*   By: sandraemiko<sandraemiko@prof.educacao.sp.gov.br> +#+  +:+     +#+         */
 /*                                                     +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 21:10:03 by sandraemiko            #+#    #+#             */
-/*                                                         ###   ########.fr       */
+/*   Updated: 2023/02/27 21:22:10 by sandraemiko           ###   ########.fr       */
 /*                                                                                 */
 /* ******************************************************************************* */
 
 #include "libft.h"
 
-int		ft_isdigit(int c)
+int	ft_isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
     	return (1);
