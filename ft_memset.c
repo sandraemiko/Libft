7b724b99e37753c99a6f8b52a6e2bceae631d006
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-27 18:18:18 -0300

    add libft.h

--- ft_memset.c
+++ ft_memset.c
@@ -15,7 +15,7 @@ void	*ft_memset(void *s, int c, size_t n)
     unsigned char *t;
 
     t = (unsigned char *)s;
-    while(n--)
+    while((n--) > 0)
          *(t ++) = (unsigned char)c;
     return (t);
 }
