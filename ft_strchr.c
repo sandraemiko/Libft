d22e1d408dd3a467348bf42c1190942fc782763e
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-27 18:59:16 -0300

    add libft.h

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By:sandraemiko<sandraemiko@prof.educacao.sp.gov.br>  +#+  +:+     +#+         */
 /*                                                     +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 16:13:03 by sandraemiko            #+#    #+#             */
-/*   updated: 2023/02/27 21:22:10 by sandraemiko           ###   ########.fr       */
+/*   Updated: 2023/02/27 21:22:10 by sandraemiko           ###   ########.fr       */
 /*                                                                                 */
 /* ******************************************************************************* */
 
