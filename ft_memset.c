ba2a0f944f98f8e5a5063817192497f9de09fa89
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-27 18:42:58 -0300

    add libft.h

--- ft_memset.c
+++ ft_memset.c
@@ -6,9 +6,11 @@
 /*   By: sandraemiko<sandraemiko@prof.educacao.sp.gov.br> +#+  +:+     +#+         */
 /*                                                     +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 20:03:03 by sandraemiko            #+#    #+#             */
-/*                                                         ###   ########.fr       */
+/*   updated: 2023/02/27 21:22:10 by sandraemiko           ###   ########.fr       */
 /*                                                                                 */
 /* ******************************************************************************* */
+
+#include "libft.h"
 
 void	*ft_memset(void *s, int c, size_t n)
 {
