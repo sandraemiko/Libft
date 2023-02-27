e3dc63e228fa80b198970a5034055377f106aca9
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-27 19:00:47 -0300

    add libft.h

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,9 +6,11 @@
 /*   By: sandraemiko<sandraemiko@prof.educacao.sp.gov.br> +#+  +:+     +#+         */
 /*                                                     +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 13:23:04 by sandraemiko            #+#    #+#             */
-/*                                                         ###   ########.fr       */
+/*   Updated: 2023/02/27 21:22:10 by sandraemiko           ###   ########.fr       */
 /*                                                                                 */
 /* ******************************************************************************* */
+
+#include "libft.h" 
 
 char	*ft_strdup(const char *s)
 {
