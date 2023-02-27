52c85e043787aa6f9cb5777cf48c93519fdd3c40
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-27 18:33:20 -0300

    add libft.h

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -5,10 +5,12 @@
 /*                                                         +:+ +:+         +:+     */
 /*   By:sandraemiko<sandraemiko@prof.educacao.sp.gov.br>  +#+  +:+     +#+         */
 /*                                                     +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/12 20:34:10 by sandraemiko            #+#    #+#             */
+/*   Created: 2023/02/27 21:14:10 by sandraemiko            #+#    #+#             */
 /*                                                         ###   ########.fr       */
 /*                                                                                 */
 /* ******************************************************************************* */
+
+#include "libft.h"
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
