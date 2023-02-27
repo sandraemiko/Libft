2b864c2e5cb8df9630c3fc1b339db2f34489fc6a
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-27 19:31:42 -0300

    add libft.h

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,11 +6,13 @@
 /*   By:sandraemiko<sandraemiko@prof.educacao.sp.gov.br>  +#+  +:+     +#+         */
 /*                                                     +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 15:31:03 by sandraemiko            #+#    #+#             */
-/*                                                         ###   ########.fr       */
+/*   Updated: 2023/02/27 21:22:10 by sandraemiko           ###   ########.fr       */
 /*                                                                                 */
 /* ******************************************************************************* */
 
-int		ft_toupper(int c)
+#include "libft.h"
+
+int	ft_toupper(int c)
 {
 	if (c >= 97 && c <= 122)
 		return (c - ' ');
