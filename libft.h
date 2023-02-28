4dd3630cec56d9f1bedd9a6c1d6bac92d1e011a5
Sandra <sandra_emiko@Alfa.localdomain>
2023-02-28 14:04:29 -0300

    add libft.h

--- libft.h
+++ libft.h
@@ -18,7 +18,7 @@
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
-int		ft_isalnum(int c);
+int		ft_isalnum(int c); 
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
