ec9e667ebbb3ff16fa2f2034e97f283636d7a5d2
sandraemiko <sandraemiko@prof.educacao.sp.gov.br>
2023-03-16 20:43:41 -0300

    ft_lstmap.c

---
+++ ft_lstiter.c
@@ -0,0 +1,27 @@
+/* ****************************************************************************** */
+/*                                                                                */
+/*                                                            :::      ::::::::   */
+/*   ft_lstiter                                             :+:      :+:    :+:   */
+/*                                                        +:+ +:+         +:+     */
+/*   By:sandraemiko<sandraemiko@prof.educacao.sp.gov.br> +#+  +:+     +#+         */
+/*                                                    +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 17:23:03 by sandraemiko           #+#    #+#             */
+/*   Updated: 2023/02/16 17:23:03 by sandraemiko          ###   ########.fr       */
+/*                                                                                */
+/* ****************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstiter(t_list *lst, void (*f)(void *))
+{
+    t_list *ptr;
+
+    if(lst == NULL)
+        return;
+    ptr = lst;
+    while (ptr != NULL)
+    {
+        f(ptr->content);
+        ptr = ptr ->next;
+    }
+}
