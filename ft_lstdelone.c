7268ce9c55ea42a0a6e18acf85148f6541d4de33
sandraemiko <sandraemiko@prof.educacao.sp.gov.br>
2023-03-16 19:10:03 -0300

    add ft_lstdelone

---
+++ ft_lstdelone.c
@@ -0,0 +1,22 @@
+/* ****************************************************************************** */
+/*                                                                                */
+/*                                                            :::      ::::::::   */
+/*   ft_lstdelone                                           :+:      :+:    :+:   */
+/*                                                        +:+ +:+         +:+     */
+/*   By:sandraemiko<sandraemiko@prof.educacao.sp.gov.br> +#+  +:+     +#+         */
+/*                                                    +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 20:42:03 by sandraemiko           #+#    #+#             */
+/*   Updated: 2023/02/16 20:42:03 by sandraemiko          ###   ########.fr       */
+/*                                                                                */
+/* ****************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+    if (lst == NULL)
+        return;
+    del(lst->content);
+    free(lst);
+    lst = NULL;
+}
