85f8c803096f3b964717e13b1a36528bda2519e7
sandraemiko <sandraemiko@prof.educacao.sp.gov.br>
2023-03-25 10:42:22 -0300

    correction

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: sandraemiko <sandraemiko@prof.educacao.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 02:54:51 by sandraemiko       #+#    #+#             */
-/*   Updated: 2023/03/21 03:20:31 by sandraemiko      ###   ########.fr       */
+/*   Updated: 2023/03/25 10:40:35 by sandraemiko      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	new->next = *lst;     
-	*lst = new;  
+	*lst = new;     
 } 
